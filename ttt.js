493ee6c8ee9d59a9674582a10d41f97dc9e15f9e
Rui Moreira <ruizinho@Mac.home.local>
2025-06-16 15:06:39 +0200

    This is challenging. Moving slowly. Hace included function to determine which cells are open to

--- ttt.js
+++ ttt.js
@@ -1,15 +1,31 @@
-function Gameboard() {
-  const rows = 3;
-  const columns = 3;
-  const board = [];
+function Gameboard (){
+    const columns = 3;
+    const rows = 3;
+    const board = [];
+    
 
-  for(let i=0;i<rows;i++){
-    board[i]=[];
-    for(let j = 0; j<columns; j++){
-        board[i].push(0);
+    for(let i=0;i<rows;i++){
+        board[i]=[];
+        for(let j=0;j<columns;j++){
+            board[i].push(0);
+        }
     }
-  }
-  return board;
-}
 
-console.log(Gameboard())
+   const checkSpace = (row,cell) =>{
+      if(board[row][cell]===0){
+        let stat = "free";
+        return stat;
+      };
+      if(board[row][cell] === "X" || board[row][cell] === "O" ){
+        let stat = "not free";
+        return stat;
+      }
+   }
+console.log(board)
+}
+Gameboard()
+
+
+
+
+  
