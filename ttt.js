fa0cac5305accfe7106a74bc4fcf2a9ee9f5b834
Rui Moreira <ruizinho@Mac.home.local>
2025-06-17 15:15:03 +0200

    Create new function to take players input, and play either 'X' or 'O' and also to check if this move is valid, ie. whether there is already a move in that spott

--- ttt.js
+++ ttt.js
@@ -1,6 +1,6 @@
 function Gameboard() {
 
-let board = [0,0,0,0,0,0,0,0,0]
+let board = ['','','','','','','','','']
 
 const players = (player,icon) =>{
    return player,icon;
@@ -9,10 +9,28 @@ const players = (player,icon) =>{
 let player1 = players("Player1","X")
 let player2 = players("Player2","O")
 
-
-
 return {board,player1,player2};
 
 }
 
-console.log(Gameboard())
+function Playing(){
+  const x =Gameboard().board
+  const player1 = Gameboard().player1
+  const player2 = Gameboard().player2
+   const check = (tray,player,cell) =>{
+    if(tray[cell]==='' && player === 'X'){
+    tray.splice(cell,1,player);
+   }
+   if(tray[cell]==='' && player === 'O'){
+    tray.splice(cell,1,player);
+   }
+  else if(tray[cell]==="X" || tray[cell]==="O"){
+    console.log("not free");
+  }};
+
+check(x,player1,2)
+check(x,player2,1)
+console.log(x)
+
+}
+Playing()
