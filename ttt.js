7db754a32d0235f116ecd7c806be404089e97763
Rui Moreira <ruizinho@Mac.home.local>
2025-06-20 11:45:58 +0200

    HAd to change code a bit. Initially I had tried out the code with prompts. Now I had to change that to event listeners. Also found a way to make factory function better and more useful.

--- ttt.js
+++ ttt.js
@@ -13,76 +13,26 @@ return {board,player1,player2};
 
 }
 
-function Playing(){
-  const x = Gameboard().board
-  const player1 = Gameboard().player1
-  const player2 = Gameboard().player2
-   const check = (tray,player,cell) =>{
-    if(tray[cell]==='' && player === 'X'){
-    tray.splice(cell,1,player);
-   }
-   if(tray[cell]==='' && player === 'O'){
-    tray.splice(cell,1,player);
-   }
-  else if(tray[cell]==="X" || tray[cell]==="O"){
-    console.log("not free");
-  }};
+function Playing(tray) {
 
+const check = (player,cell) =>{
+    if (tray[cell] === '' && (player === 'X' || player === 'O')) {
+      tray.splice(cell, 1, player);
+    } else if (tray[cell] === 'X' || tray[cell] === 'O') {
+      console.log('not free');
+    }
+  };
 
-console.log(x)
-const flow = () =>{
-  let turn = player1;
-  if(turn = player1){
-  moveOne = prompt("Player 1, where do you wanna play?")
-  check(x,player1,moveOne)}
-  if(x[0]=="X" && x[1]=="X" && x[2]=="X"||
-    x[0]=="X" && x[3]=="X" && x[6]=="X"||
-    x[2]=="X" && x[5]=="X" && x[8]=="X"||
-    x[6]=="X" && x[7]=="X" && x[8]=="X"||
-    x[1]=="X" && x[4]=="X" && x[7]=="X"||
-    x[3]=="X" && x[4]=="X" && x[5]=="X"||
-    x[0]=="X" && x[4]=="X" && x[8]=="X"||
-    x[6]=="X" && x[4]=="X" && x[2]=="X"){
-    console.log(x)  
-    console.log("Player 1 wins!")
-    return;
-  }
-  else if(!x.includes('')){
-    console.log("Tie")
-    return
-  }
-  if(turn = player2){
-  moveTwo = prompt("Player 2, where do you wanna play?")
-  check(x,player2,moveTwo)}
-  if(x[0]=="O" && x[1]=="O" && x[2]=="O"||
-     x[0]=="O" && x[3]=="O" && x[6]=="O"||
-     x[2]=="O" && x[5]=="O" && x[8]=="O"||
-     x[6]=="O" && x[7]=="O" && x[8]=="O"||
-     x[1]=="O" && x[4]=="O" && x[7]=="O"||
-     x[3]=="O" && x[4]=="O" && x[5]=="O"||
-     x[0]=="O" && x[4]=="O" && x[8]=="O"||
-     x[6]=="O" && x[4]=="O" && x[2]=="O"){
-      console.log(x) 
-    console.log("Player 2 wins!")
-    return;
-  }
-  else if(!x.includes('')){
-    console.log("Tie")
-    return
-  }
-  
-  if(x.includes('')){
-    flow();
-  }
-  
-  
+return{check}
 }
-flow()
-return{x}
-}
+
 
 function Display(){
- const board = Playing().x;
+ const board = Gameboard().board;
+ const p1=Gameboard().player1;
+ const p2=Gameboard().player2;
+ const play = Playing(board)
+ 
  const div = document.createElement("div");
  document.body.appendChild(div);
  div.id = "game"
@@ -102,10 +52,42 @@ function Display(){
   <div id="cell7"> ${board[6]}</div>
   <div id="cell8"> ${board[7]}</div>
   <div id="cell9"> ${board[8]}</div>
-  </div>`;
+  </div>`+
+  `<div id="display"></div>`;
 
+  const input = () =>{
+  let turn = p1;
+  for(let i = 0; i<board.length;i++){
+  const cell = document.querySelector(`#cell${i+1}`);
+  cell.addEventListener("click",()=>{
+    if(turn === p1){
+      play.check(turn,i)
+      cell.textContent=board[i];
+      turn = p2
+      console.log(board)
+    }
+    else if(turn === p2){
+      play.check(turn,i)
+      cell.textContent=board[i];
+      turn = p1;
+      console.log(board)
+    }
+  })
+
+  }
+  }
+  return{input}
 }
 
-Display();
+
+function winning(){
+  let display = Display()
+  let type = display.input()
+
+  return type;
+}
+
+winning()
+
 
 
