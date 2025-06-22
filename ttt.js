4b221729be367d29142590fec0f93eafc5c37d06
Rui Moreira <ruizinho@Mac.home.local>
2025-06-22 11:49:27 +0200

    Finally finished with the project. Users can change their names, there is a button to start/restart the game and results are displayed at the end as well as whose turn it is

--- ttt.js
+++ ttt.js
@@ -1,15 +1,15 @@
 function Gameboard() {
 
-let board = ['','','','','','','','','']
+let board = ['','','','','','','','',''];
+
+
 
 const players = (player,icon) =>{
-   return player,icon;
+   return {player,icon};
 }
-
-let player1 = players("Player1","X")
-let player2 = players("Player2","O")
-
-return {board,player1,player2};
+let player1=players("Player 1",'X')
+let player2=players("Player 2",'O')
+return {board,players,player1,player2};
 
 }
 
@@ -33,7 +33,6 @@ const win = (board)=>{
     board[3]=="X" && board[4]=="X" && board[5]=="X"||
     board[0]=="X" && board[4]=="X" && board[8]=="X"||
     board[6]=="X" && board[4]=="X" && board[2]=="X"){
-    console.log("Player 1 wins");
     Object.freeze(board);
 
   }
@@ -50,7 +49,6 @@ const win = (board)=>{
      board[3]=="O" && board[4]=="O" && board[5]=="O"||
      board[0]=="O" && board[4]=="O" && board[8]=="O"||
      board[6]=="O" && board[4]=="O" && board[2]=="O"){
-    console.log("Player 2 wins!");
     Object.freeze(board);
   }
   else if(!board.includes('')){
@@ -65,13 +63,11 @@ return{check,win}
 
 function Display(){
  const board = Gameboard().board;
- const p1=Gameboard().player1;
- const p2=Gameboard().player2;
+ const pa=Gameboard().player1;
+ const p1 = pa.icon
+ const pb=Gameboard().player2;
+ const p2 = pb.icon
  const play = Playing(board);
-
- const div = document.createElement("div");
- document.body.appendChild(div);
- div.id = "game"
 
  document.getElementById(`game`).innerHTML=
   `<div id="row1">
@@ -88,13 +84,15 @@ function Display(){
   <div id="cell7"> ${board[6]}</div>
   <div id="cell8"> ${board[7]}</div>
   <div id="cell9"> ${board[8]}</div>
-  </div>`+
-  `<div id="display"></div>`;
+  </div>`;
+  
 
   const input = () =>{
   let turn = p1;
+  let name1 = winning().p1;
+  let name2 = winning().p2;
   const screen = document.querySelector("#display")
-  screen.textContent="Player 1's turn!"
+  screen.textContent=`It's ${name1.player}'s turn!`
   for(let i = 0; i<board.length;i++){
   const cell = document.querySelector(`#cell${i+1}`);
   cell.addEventListener("click",()=>{
@@ -103,14 +101,16 @@ function Display(){
       cell.textContent=board[i];
       turn = p2
       play.win(board)
-      console.log(board)
       
       if(Object.isFrozen(board)){
         turn = "Player 1";
-        screen.textContent=`Game Over! ${turn} wins!`
+        screen.textContent=`Game Over! ${name1.player} wins!`
+      }
+       else if(!board.includes('')){
+        screen.textContent="It's a tie!"
       }
       else{
-        screen.textContent="Player 2's turn!"
+        screen.textContent=`It's ${name2.player}'s turn!`
       }
       
     }
@@ -118,14 +118,17 @@ function Display(){
       play.check(turn,i)
       cell.textContent=board[i];
       turn = p1;
-      console.log(board)
+
       play.win(board)
       if(Object.isFrozen(board)){
         turn = "Player 2"
-        screen.textContent=`Game Over! ${turn} wins!`
+        screen.textContent=`Game Over! ${name2.player} wins!`
+      }
+       else if(!board.includes('')){
+        screen.textContent="It's a tie!"
       }
       else{
-        screen.textContent="Player 1's turn!"
+        screen.textContent=`It's ${name1.player}'s turn!`
       }
   
 
@@ -139,12 +142,26 @@ function Display(){
 
 
 function winning(){
+let player1 = Gameboard().players;
+let player2 = Gameboard().players;
+const modal = document.querySelector(".modal");
+const btn = document.querySelector(".btn")
+
+btn.addEventListener("click",()=>{
+  modal.show()
+  const submit = document.querySelector("#submit");
+  submit.addEventListener("click",()=>{
+  p1 = player1(`${document.getElementById("one").value}`,'X')
+  p2 = player2(`${document.getElementById("two").value}`,'O')
   let display = Display()
   let type = display.input()
-  
-  return type;
-  
+   return {type,p1,p2};}
+  )})
+return {p1,p2}
+
 }
+  
+  
 
 winning()
 
